0315660d717b83b522e0544ffa5d392ce587c913
Chris Craig <chricrai423@gmail.com>
2019-09-17 11:48:17 -0400

    I can check for binaries and exec bash

--- CLock.cpp
+++ CLock.cpp
@@ -1,14 +1,61 @@
-//#include <stdlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
 #include <ctime>
 
+std::string ex_ec(const char* cmd){
+    FILE* pipe;
+	char path[128];
+	std::string result = "";
+
+	/* open */
+	pipe = popen(cmd, "r");
+	if (pipe == NULL) {
+	    printf("Failed to run command\n" );
+	    exit(1);
+	}
+
+	/* Read the output a line at a time - output it. */
+	while (fgets(path, sizeof(path)-1, pipe) != NULL) {
+		//printf("%s", path);
+		result += path;
+	}
+
+	/* close */
+	pclose(pipe);
+	return result;
+}
+
+bool contains(std::string in, std::string search_str){
+	size_t pos = in.find(search_str);
+	if (pos == std::string::npos) { // npos == -1
+		return false;
+	}
+	return true;
+}
+
 int main(){
     /* Check for current time */
     std::time_t t = std::time(nullptr);
-    std::tm * tm; //Datetime of target (Competition day at 12:00 EST)
-    tm = std::localtime(&t);
+    std::tm * tm; // Datetime of target (Competition day at 12:00 EST)
+    bool openssl, zip, tar, junk;
+	std::string out;
+
+
+	// check the local time 
+    tm = std::localtime(&t); 
+	
+    openssl = contains(ex_ec("which openssl"),"openssl");
+    zip = contains(ex_ec("which zip"),"zip");
+    tar = contains(ex_ec("which tar"),"tar");
+    junk = contains(ex_ec("which junk"),"junk");
+
+	std::cout << openssl << std::endl;
+	std::cout << zip << std::endl;
+	std::cout << tar << std::endl;
+
+/*
     //tm->tm_year = 2019;
     //tm->tm_mon = 11;
     //tm->tm_mday = 30;
@@ -22,14 +69,15 @@ int main(){
         std::system("sleep 1");
         printf("sec: %d \n",tm->tm_sec % 16);
     }
-    printf("seconds past the minute: %d\n", tm->tm_sec);
-    std::cout << std::endl;
+*/
+
+    //printf("seconds past the minute: %d\n", tm->tm_sec);
+    //std::cout << std::endl;
     //tm->tm_hour = 12;
     //std::cout  << std::put_time( &tm, "%c" ) << std::endl;
-    std::cout  << std::asctime(tm) << std::endl;
+    //std::cout  << std::asctime(tm) << std::endl;
 
     /* Run various system commands as root */
-    std::system("echo done");
-
+	/* Open the command for reading. */
     return 0;
 }
