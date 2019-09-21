3089344ebd2153377f5bc2a9bcd52d0c64c0f843
Chris Craig <chricrai423@gmail.com>
2019-09-21 14:09:37 -0400

    adding install file for ease of use

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Make the binaries
+make
+
+# copy output binaries to their final destinations
+sudo cp clock /usr/local/bin/
+sudo cp clock.sh /usr/local/bin/
+sudo cp clocklib /usr/local/lib
+sudo cp profile /usr/local/bin/
+
+# set the service for your distro (systemd)
+sudo cp clock.service /etc/systemd/system
+sudo systemctl enable clock
+
